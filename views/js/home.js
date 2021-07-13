92a66a22925aacbc0db05af295887af2e7e0ff5e
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-07-13 20:19:17 +0530

    Connected To Firebase

--- views/js/home.js
+++ views/js/home.js
@@ -1,52 +1,52 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCy_VLPayq4HwcpdqXZ3X2aLy4M3B0Y5I0",
-    authDomain: "portfolio-contact-form-7d200.firebaseapp.com",
-    projectId: "portfolio-contact-form-7d200",
-    storageBucket: "portfolio-contact-form-7d200.appspot.com",
-    messagingSenderId: "1037199713838",
-    appId: "1:1037199713838:web:d21336e7b3b30413ede1bf"
-};
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDD026eNT422iw3FF8AcOmKXJhT-od3EVs",
+    authDomain: "personal-website-c568f.firebaseapp.com",
+    projectId: "personal-website-c568f",
+    storageBucket: "personal-website-c568f.appspot.com",
+    messagingSenderId: "254236089898",
+    appId: "1:254236089898:web:a383a581b010f0b6c22d53"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
+  let contactInfo = firebase.database().ref("infos");
 
-let contactInfo = firebase.database().ref("infos");
+  document.querySelector(".contact-form").addEventListener("submit", submitForm);
 
-document.querySelector(".contact-form").addEventListener("submit", submitForm);
+  function submitForm(e) {
+    e.preventDefault();
 
-function submitForm(e) {
-  e.preventDefault();
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
 
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let subject = document.getElementById("subject").value;
-  let message = document.getElementById("message").value;
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
 
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = date+' '+time;
+    saveContactInfo(name, email, subject, message, dateTime);
 
-  saveContactInfo(name, email, subject, message, dateTime);
+    let alert = document.getElementById('alert');
+    alert.innerHTML = 'Your message has been sent successfully';
 
-  let alert = document.getElementById('alert');
-  alert.innerHTML = 'Your message has been sent successfully';
+    setTimeout(() => {
+      alert.innerHTML = '';
+      document.querySelector(".contact-form").reset();
+    }, 3000)
+  }
 
-  setTimeout(() => {
-    alert.innerHTML = '';
-    document.querySelector(".contact-form").reset();
-  }, 3000)
-}
+  // Save user information to Firebase
+  function saveContactInfo(name, email, subject, message, dateTime) {
+    let newContactInfo = contactInfo.push();
 
-// Save user information to Firebase
-function saveContactInfo(name, email, subject, message, dateTime) {
-  let newContactInfo = contactInfo.push();
-
-  newContactInfo.set({
-    name: name,
-    email: email,
-    subject: subject,
-    message: message,
-    dateTime: dateTime,
-  });
-}
+    newContactInfo.set({
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+      dateTime: dateTime,
+    });
+  }
