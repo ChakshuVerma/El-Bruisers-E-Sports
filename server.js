1c3bb10beb2bcf5cfb4412c3499f3e21020645b2
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-08-18 15:18:03 +0530

    Data Changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
 
 
 // Routes
-app.use('/', require('./routes/index.js'));
+// app.use('/', require('./routes/index.js'));
 app.use('*', (req,res) => {
   res.render('404')
 })
