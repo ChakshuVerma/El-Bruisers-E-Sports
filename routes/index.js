5c96b975334e7d5e60b12de0733598cb408dbd5c
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-08-18 23:30:56 +0530

    Intial Changes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const route = require('express').Router()
 const path  = require('path')
 
 route.get('/', (req,res) => res.render('home'))
-// route.get('/tournaments/surtur_crown_championship', (req,res) => res.render('surtur_crown_championship'))
+route.get('/tournaments/surtur_crown_championship', (req,res) => res.render('surtur_crown_championship'))
 
 exports = module.exports = route;  
